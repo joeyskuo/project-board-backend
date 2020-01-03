173a6ea868ba49a368f89f4cd0369d037755887b
Joey <joeyskuo@gmail.com>
2020-01-02 22:09:11 -0800

    added project repository

---
+++ src/main/java/com/cobaltframework/projectboard/repositories/ProjectRepository.java
@@ -0,0 +1,15 @@
+package com.cobaltframework.projectboard.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cobaltframework.projectboard.domain.Project;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long>{
+
+	@Override
+	Iterable<Project> findAllById(Iterable<Long> ids);
+
+	
+}
