b2a7faa1b0eb1b9de1908d78a7ac76332ccdf465
Joey <joeyskuo@gmail.com>
2020-01-09 19:24:56 -0800

    added get project by id to project controller

--- src/main/java/com/cobaltframework/projectboard/controller/ProjectController.java
+++ src/main/java/com/cobaltframework/projectboard/controller/ProjectController.java
@@ -3,6 +3,8 @@ package com.cobaltframework.projectboard.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,5 +24,13 @@ public class ProjectController {
 	public ResponseEntity<Project> createNewProject(@RequestBody Project project) {
 		Project newProject = projectService.saveOrUpdateProject(project);
 		return new ResponseEntity<Project>(newProject, HttpStatus.CREATED);
+	}
+	
+	@GetMapping("/{projectId}")
+	public ResponseEntity<?> getProjectById(@PathVariable String projectId) {
+		
+		Project project = projectService.findProjectByIdentifier(projectId);
+		
+		return new ResponseEntity<Project>(project, HttpStatus.OK);
 	}
 }
